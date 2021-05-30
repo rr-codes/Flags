6bd7ddf52e3f1f0111d8d127cf07711e1144c726
Richard Robinson <richard@rr.codes>
2021-05-30 00:18:41 -0400

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Richard Robinson on 2021-05-30.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
